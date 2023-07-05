c3c3bf17809cf79427030e430da832efaa5d7c71
matorolll <mgajos00@gmail.com>
2023-07-05 22:15:10 +0200

    Pre second fix

--- BlazorEcommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -1,10 +1,9 @@
-﻿using BlazorEcommerce.Shared;
-
-namespace BlazorEcommerce.Client.Services.ProductService
+﻿namespace BlazorEcommerce.Client.Services.ProductService
 {
     public interface IProductService
     {
         List<Product> Products { get; set; }
         Task GetProducts();
+        Task<ServiceResponse<Product>> GetProduct(int productId);
     }
 }
--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -10,10 +10,15 @@
         }
         public List<Product> Products { get; set; } = new List<Product>();
 
+        public async Task<ServiceResponse<Product>> GetProduct(int productId)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<Product>>($"api/product/{productId}");
+            return result;
+        }
+
         public async Task GetProducts()
         {
             var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
-            Products = result.Data;
             if (result != null && result.Data != null)
                 Products = result.Data;
         }
